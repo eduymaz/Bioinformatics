4a7e807777035cde030d046d8ca874114825ea7a
Elif Duymaz Yılmaz <97849933+eduymaz@users.noreply.github.com>
2024-08-27 16:04:53 +0300

    Update figures.R

--- alternativesplicing|scRNA/figures.R
+++ alternativesplicing|scRNA/figures.R
@@ -75,6 +75,83 @@ ggplot(data_tidy, aes(x = gene_name, y = PSI, fill = Group)) +
   facet_wrap(~ Group_Label, scales = "free_y", ncol = 2)  # Multi box plot 
 
 
+####### ####### ####### ####### ####### ####### ####### ####### ####### ####### ####### ####### ####### ####### ####### 
+# Figure 4 | Heatmap
+merged_data <- rbind(data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11)
+# FDR hesaplama
+merged_data$FDR <- p.adjust(merged_data$p_value, method = "BH")
+selected_genes <- c("AMD1", "ARMC8", "ATP6V1C2", "TDP1", "THOC2", "UBAP2L")
+scats_results <- read_excel("/Users/edyilmaz/Desktop/embryo/results/smaller_than_0.05_pvalues_new2.xlsx", sheet=1)
 
+####################### Fig 4| Heatmap with diagonal 
 
+#FILTERED_DATA
+filtered_data <- merged_data %>%
+  filter(gene_name %in% selected_genes)
+#write.csv(filtered_data, "filtered_data.csv", row.names = FALSE)
+#filtered_data <- read.csv("filtered_data.csv")
 
+# PLOT CODES: 
+library(corrplot)
+library(ggplot2)
+library(reshape2)
+library(ComplexHeatmap)
+library(circlize)
+library(dplyr)
+library(tidyr)
+
+AMD1_data <- filtered_data %>% filter(gene_name == "AMD1")
+ARMC8_data <- filtered_data %>% filter(gene_name == "ARMC8")
+ATP6V1C2_data <- filtered_data %>% filter(gene_name == "ATP6V1C2")
+TDP1_data <- filtered_data %>% filter(gene_name == "TDP1")
+THOC2_data <- filtered_data %>% filter(gene_name == "THOC2")
+UBAP2L_data <- filtered_data %>% filter(gene_name == "UBAP2L")
+
+##########
+
+#data: AMD1_data | ARMC8_data | ATP6V1C2_data | TDP1_data | THOC2_data | UBAP2L_data
+data <- UBAP2L_data
+  
+# calculate average values
+average_values <- data %>%
+  group_by(gp1, gp2) %>%
+  summarize(
+    avg_PSI_gp1 = mean(PSI_gp1, na.rm = TRUE),
+    avg_PSI_gp2 = mean(PSI_gp2, na.rm = TRUE)
+  ) %>%
+  ungroup()
+
+# 
+unique_gp <- unique(c(average_values$gp1, average_values$gp2))
+unique_gp <- sort(unique_gp) 
+
+#make a matrix
+heatmap_matrix <- matrix(NA, nrow = length(unique_gp), ncol = length(unique_gp))
+rownames(heatmap_matrix) <- unique_gp
+colnames(heatmap_matrix) <- unique_gp
+
+# put the average values in map
+for (i in unique_gp) {
+  for (j in unique_gp) {
+    avg1 <- average_values %>%
+      filter(gp1 == i & gp2 == j) %>%
+      summarize(avg = mean(avg_PSI_gp2, na.rm = TRUE)) %>%
+      pull(avg)
+    avg2 <- average_values %>%
+      filter(gp1 == j & gp2 == i) %>%
+      summarize(avg = mean(avg_PSI_gp1, na.rm = TRUE)) %>%
+      pull(avg)
+    if (is.na(avg1) && is.na(avg2)) {
+      heatmap_matrix[i, j] <- NA
+    } else {
+      heatmap_matrix[i, j] <- mean(c(avg1, avg2), na.rm = TRUE)
+    }
+  }
+}
+
+# Make a correlation matrx
+numeric_data <- heatmap_matrix
+correlation_matrix <- cor(numeric_data, use = "pairwise.complete.obs", method = "pearson")
+
+corrplot(correlation_matrix, method = "color", type = "full", tl.col = "black", tl.srt = 45, 
+         addCoef.col = "black", number.cex = 0.1)
